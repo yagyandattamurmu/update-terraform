63ea1311b21c58a6c3969fcf0f37ee198ab2f427
yagyandattamurmu <yagyandatta.murmu@cozentus.com>
2025-09-15 14:44:56 +0530

    Add  variables.tf from source repo

--- variables.tf
+++ variables.tf
@@ -1,1 +1,15 @@
+variable "project_name" {
+  description = "value to identify project"
+  type        = string
+}
 
+variable "env" {
+  description = "Environment"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-west-2"
+}
